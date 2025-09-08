76c30299664373cb22cd506234000c49b7fd19fc
Julien A Nguinkal <37578252+bbalog87@users.noreply.github.com>
2025-09-08 11:31:44 +0200

    Add files via upload

---
+++ create_mpx_nextstrain_structure.sh
@@ -0,0 +1,24 @@
+
+#!/usr/bin/bash
+
+# Create base directory
+mkdir -p mpx_nextstrain
+
+# Data folders
+mkdir -p mpx_nextstrain/data/genomes
+
+# Reference folders
+mkdir -p mpx_nextstrain/ref
+
+# Config folder
+mkdir -p mpx_nextstrain/config
+
+# Builds folder
+mkdir -p mpx_nextstrain/builds/mpx_africa
+
+# Env folder
+mkdir -p mpx_nextstrain/env
+
+echo "📂 mpx_nextstrain project structure created."
+echo "You can now add your data, configuration files,
+ and environment settings."
